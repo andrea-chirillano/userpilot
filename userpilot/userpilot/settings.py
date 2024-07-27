92d10949f6051963568934abd95ede2ee1c94b80
Andrea Chirillano <andreatchirillano@hotmail.com>
2024-07-27 18:29:46 -0300

    Add: docker conteiner

--- userpilot/userpilot/settings.py
+++ userpilot/userpilot/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'userpilot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'userpilot',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
